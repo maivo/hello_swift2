4cdfd7532fdbe5a10d4685a8e811603fb1cca530
phonger <phonger@USERs-MacBook-Pro.local>
2015-07-15 14:41:26 -0600

    Initial Commit

---
+++ hello_swift2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello_swift2
+//
+//  Created by phonger on 2015-07-15.
+//  Copyright (c) 2015 phonger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
